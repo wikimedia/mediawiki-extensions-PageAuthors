662567021296fe3ceabebe34b65c10637d0dbc8f
Sophivorus <schenonef@gmail.com>
2021-10-18 12:41:49 -0700

    Inital commit
    Change-Id: Iaba1217c8eb16b45deed95185dd942237bc429ed

---
+++ PageAuthors.i18n.magic.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Internationalization file for the magic word of the PageAuthors extension.
+ *
+ * @file PageAuthors.i18n.magic.php
+ * @ingroup PageAuthors
+ * @author Sophivorus
+ */
+
+$magicWords = [];
+
+/** English
+ * @author Sophivorus
+ */
+$magicWords['en'] = [
+	'PAGEAUTHORS' => [ 0, 'PAGEAUTHORS' ]
+];
---
+++ PageAuthors.php
@@ -0,0 +1,103 @@
+<?php
+
+use MediaWiki\MediaWikiServices;
+
+class PageAuthors {
+
+	/**
+	 * Register PAGEAUTHORS variable
+	 */
+	public static function onGetMagicVariableIDs( &$variableIDs ) {
+		$variableIDs[] = 'PAGEAUTHORS';
+	}
+
+	/**
+	 * Register PAGEAUTHORS parser function
+	 */
+	public static function onParserFirstCallInit( Parser $parser ) {
+		$parser->setFunctionHook( 'PAGEAUTHORS', [ self::class, 'getPageAuthors' ], SFH_NO_HASH );
+	}
+
+	/**
+	 * Assign a value to PAGEAUTHORS variable
+	 */
+	public static function onParserGetVariableValueSwitch( $parser, &$variableCache, $magicWordId, &$ret, $frame ) {
+		if ( $magicWordId === 'PAGEAUTHORS' ) {
+			$ret = self::getPageAuthors( $parser );
+		}
+		return true;
+	}
+
+	/**
+	 * Get the main authors of the given page
+	 */
+	public static function getPageAuthors( Parser $parser, string $input = '' ) {
+		global $wgPageAuthorsDelimiter,
+			$wgPageAuthorsMinBytesPerEdit,
+			$wgPageAuthorsMinBytesPerAuthor,
+			$wgPageAuthorsIgnoreSummaryPatterns,
+			$wgPageAuthorsIgnoreMinorEdits,
+			$wgPageAuthorsIgnoreSystemUsers,
+			$wgPageAuthorsIgnoreBots,
+			$wgPageAuthorsIgnoreBlocked, 
+			$wgPageAuthorsIgnoreAnons;
+		$title = $input ? Title::newFromText( $input ) : $parser->getTitle();
+		$id = $title->getArticleID();
+		$authors = [];
+		$dbr = wfGetDB( DB_REPLICA );
+		$revisionSize = 0;
+		$revisionIds = $dbr->selectFieldValues( 'revision', 'rev_id', 'rev_page = ' . $id );
+		$revisionStore = MediaWikiServices::getInstance()->getRevisionStore();
+		foreach ( $revisionIds as $revisionId ) {
+			$revision = $revisionStore->getRevisionById( $revisionId );
+			if ( !$revision ) {
+				continue;
+			}
+			$revisionDiff = $revision->getSize() - $revisionSize;
+			$revisionSize = $revision->getSize();
+			if ( $revisionDiff < $wgPageAuthorsMinBytesPerEdit ) {
+				continue;
+			}
+			if ( $wgPageAuthorsIgnoreMinorEdits && $revision->isMinor() ) {
+				continue;
+			}
+			if ( $wgPageAuthorsIgnoreSummaryPatterns ) {
+				$comment = $revision->getComment();
+				$summary = $comment->text;
+				foreach ( $wgPageAuthorsIgnoreSummaryPatterns as $pattern ) {
+					if ( preg_match( $pattern, $summary ) ) {
+						continue 2;
+					}
+				}
+			}
+			$revisionUser = $revision->getUser();
+			if ( $wgPageAuthorsIgnoreSystemUsers && $revisionUser->isSystemUser() ) {
+				continue;
+			}
+			if ( $wgPageAuthorsIgnoreBots && $revisionUser->isBot() ) {
+				continue;
+			}
+			if ( $wgPageAuthorsIgnoreBlocked && $revisionUser->isBlocked() ) {
+				continue;
+			}
+			if ( $wgPageAuthorsIgnoreAnons && $revisionUser->isAnon() ) {
+				continue;
+			}
+			$revisionUserName = $revisionUser->getName();
+			if ( array_key_exists( $revisionUserName, $authors ) ) {
+				$authors[ $revisionUserName ] += $revisionDiff;
+			} else {
+				$authors[ $revisionUserName ] = $revisionDiff;
+			}
+		}
+		$authors = array_filter( $authors, function ( $bytes ) {
+			global $wgPageAuthorsMinBytesPerAuthor;
+			return $wgPageAuthorsMinBytesPerAuthor < $bytes;
+		} );
+		arsort( $authors );
+		$authors = array_keys( $authors );
+		$authors = implode( $wgPageAuthorsDelimiter, $authors );
+		return $authors;
+	}
+}
+ 
